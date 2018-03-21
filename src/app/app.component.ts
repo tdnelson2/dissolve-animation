a0547c3008dfdf24500e080866c4626fa9e10362
Tim Nelson <tdnelson@gmail.com>
2018-03-21 16:21:09 -0700

    complete working photo cross dissolve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,51 +2,97 @@ import { Component, OnInit, Input } from '@angular/core';
 import { style, transition, animate, trigger, state } from '@angular/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { Observable } from 'rxjs/Observable';
+
+import { PhotoService } from './photo.service';
+
+  // template: `<h1>This is some text</h1>
+  //            <div class="solid"></div>
+  //            <img class="bg-img" [@crossfade]="state1" src="{{photoA.url}}">
+  //            <img class="bg-img" [@crossfade]="state2" src="{{photoB.url}}">`,
+
 @Component({
   selector: 'app-root',
-  template: `<!-- class title sets both to the same absolute location -->
-             <h1 class="title" [@crossfade]="state1">{{title1}}</h1>
-             <h1 class="title" [@crossfade]="state2">{{title2}}</h1>`,
+  template: `<h1>This is some text</h1>
+             <!-- <div class="solid"></div> -->
+             <div class="bg" [@crossfade]="state1" [ngStyle]="photoA.style"></div>
+             <div class="bg" [@crossfade]="state2" [ngStyle]="photoB.style"></div>`,
   animations: [
           trigger('crossfade', [
               state('show', style({ opacity: 1 })),
               state('hide', style({ opacity: 0 })),
-              transition('* => show', animate('1s ease-in')),
-              transition('show => hide', animate('1s ease-out'))
-          ])]
+              transition('* => show', animate('3s linear')),
+              transition('show => hide', animate('3s linear'))
+          ])],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
 
-    title1 = 'BOOM!';
-    title2 = 'POP!';
-    state1 = 'hide';
-    state2 = 'hide';
-    titles = ['POW', 'BLAST', 'RUMBLE', 'SHATTER']
-    i = 0;
+  photos: any[] = [];
+  photoA: any;
+  photoB: any;
+  state1 = 'show';
+  state2 = 'hide';
+  i = 1;
 
-     switchTitles(newTitle) {
-        if (this.state1 === 'show') {
-            this.title2 = newTitle;
-            this.state1 = 'hide';
-            this.state2 = 'show';
-        } else {
-            this.title1 = newTitle;
-            this.state2 = 'hide';
-            this.state1 = 'show';
-        }
+   switchPhotos(nextPhoto) {
+      if (this.state1 === 'show') {
+          this.state1 = 'hide';
+          this.state2 = 'show';
+          setTimeout(() => {
+            this.photoA = nextPhoto;
+          }, 3000);
+      } else {
+          this.state2 = 'hide';
+          this.state1 = 'show';
+          setTimeout(() => {
+            this.photoB = nextPhoto;
+          }, 3000);
+      }
+  }
+
+  constructor( private photoService: PhotoService ) { }
+
+  ngOnInit() {
+    this.photoService.fetchPhotos().subscribe((response: any) => {
+      const start = this.getRandomInt(0,response.length-11);
+      this.photos = response.slice(start, start+10);
+      this.addData();
+      this.queuePhotos();
+    });
+  }
+
+  private buildImgUrl(id) {
+    const width = window.innerWidth*window.devicePixelRatio;
+    const height = window.innerHeight*window.devicePixelRatio;
+    return `https://picsum.photos/${width}/${height}/?image=${id}`
+  }
+
+  private addData() {
+    for (let photo of this.photos) {
+      photo['style'] = this.buildBGStyle(photo.id);
     }
+  }
 
-    constructor( ) { }
 
-    ngOnInit() { this.queueStories() }
+  private getRandomInt(min: number, max:number) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-  queueStories(): void {
+  private queuePhotos(): void {
+    this.photoA = this.photos[0];
+    this.photoB = this.photos[1];
 
-      setInterval(() => {
-        this.i = this.i === this.titles.length-1 ? 0 : this.i+1;
-        this.switchTitles(this.titles[this.i]);
-        console.log(this.titles[this.i]);
-      },4000);
+    setInterval(() => {
+      this.i = this.i === this.photos.length-1 ? 0 : this.i+1;
+      this.switchPhotos(this.photos[this.i]);
+      console.log(this.photos[this.i]);
+    },8000);
+  }
+
+  private buildBGStyle(id) {
+    const url = this.buildImgUrl(id);
+    return {'background-image':  `url(${url})`};
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule }        from '@angular/common/http';
+
+import { PhotoService }                       from './photo.service';
 
 
 import { AppComponent } from './app.component';
@@ -12,9 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    PhotoService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
