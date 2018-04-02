a17bf0589db84f8ee35d16b261a6573678933db5
Tim Nelson <tdnelson@gmail.com>
2018-04-02 14:49:01 -0700

    include nyt key

--- example-project-angular/src/app/nytimes.service.ts
+++ example-project-angular/src/app/nytimes.service.ts
@@ -3,12 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 
-import { NYTKEY } from '../../../../SECRETS/nyt-key';
+// import { NYTKEY } from '../../../../SECRETS/nyt-key';
 
 @Injectable()
 export class NytimesService {
   private nytURL =
-  `https://api.nytimes.com/svc/news/v3/content/all/business%20day.json?api-key=${NYTKEY}`;
+  `https://api.nytimes.com/svc/news/v3/content/all/business%20day.json?api-key=6a6abed21d7d4ab081909e80793e5bdd`;
 
   constructor(private http: HttpClient) {}
 
