2607c442717b225b1af08bf0fd9a22e79f6fd885
Tim Nelson <tdnelson@gmail.com>
2018-04-05 14:41:23 -0700

    initial commit

--- src/transition-item.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TransitionItem {
-    track: number;
-    state: string;
-    klass: string;
-    data: any;
-    constructor(track: number, state: string, klass: string, data: string);
-}
